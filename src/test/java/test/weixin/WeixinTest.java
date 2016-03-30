8cc0625e3d678ef67c370bd72d89df09c3a16120
eryan <eryanwcp@gmail.com>
2016-03-30 22:48:37 +0800

    bug 修复

--- src/test/java/test/weixin/WeixinTest.java
+++ src/test/java/test/weixin/WeixinTest.java
@@ -102,75 +102,14 @@ public class WeixinTest {
         noitceMenu3.setName("主页");
         noitceMenu3.setKey("menu_main");
         noitceMenu3.setType(QYMenuType.VIEW);
-//        https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx9af4b4425e654a20&redirect_uri=http://www.jfit.com.cn/labor_dev/m/qyweixin/index&response_type=code&scope=snsapi_base&state=1#wechat_redirect
-        noitceMenu3.setUrl("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx9af4b4425e654a20&redirect_uri=http://www.jfit.com.cn/labor_dev/m/qyweixin/index&response_type=code&scope=snsapi_base&state=1#wechat_redirect");
-//        noitceMenu3.setUrl("http://www.jfit.com.cn/labor_dev/m/qyweixin/index");
+        String url = "http://www.jfit.com.cn/labor_dev/m/qyweixin/index";
+        noitceMenu3.setUrl(WeixinUtils.getOauth2URL(url));
         noitceMenus.add(noitceMenu3);
 
         noitceMenu.setSubButton(noitceMenus);
 
 
-        QYMenuButton libMenu = new QYMenuButton();
-        libMenu.setName("我的图书");
-        libMenu.setKey("menu_lib");
-        libMenu.setType(QYMenuType.CLICK);
-        List<QYMenuButton> libMenus = Lists.newArrayList();
-
-        QYMenuButton libMenu1 = new QYMenuButton();
-        libMenu1.setType(QYMenuType.SCANCODE_PUSH);
-        libMenu1.setName("扫一扫");
-        libMenu1.setKey("menu_libMenu1");
-        libMenus.add(libMenu1);
-
-        QYMenuButton libMenu3 = new QYMenuButton();
-        libMenu3.setType(QYMenuType.VIEW);
-        String toURL = "http://www.jfit.com.cn/labor_lyr/m/qyweixin/labor/qr";
-        libMenu3.setName("扫一扫（跳转）");
-        libMenu3.setKey("menu_libMenu3");
-        libMenu3.setUrl(WeixinUtils.getOauth2URL(toURL));
-        libMenus.add(libMenu3);
-
-        QYMenuButton libMenu2 = new QYMenuButton();
-        libMenu2.setType(QYMenuType.CLICK);
-        libMenu2.setName("我的图书");
-        libMenu2.setKey("menu_libMenu2");
-        libMenus.add(libMenu2);
-
-        QYMenuButton libMenu4 = new QYMenuButton();
-        libMenu4.setType(QYMenuType.VIEW);
-        String toURL4 = "http://www.jfit.com.cn/labor_lyr/m/libary";
-        libMenu4.setName("图书搜索");
-        libMenu4.setKey("menu_libMenu4");
-        libMenu4.setUrl(WeixinUtils.getOauth2URL(toURL4));
-        libMenus.add(libMenu4);
-
-        libMenu.setSubButton(libMenus);
-
-
-        QYMenuButton countMenu = new QYMenuButton();
-        countMenu.setName("积分");
-        countMenu.setKey("menu_countMenu");
-        countMenu.setType(QYMenuType.CLICK);
-        List<QYMenuButton> countMenus = Lists.newArrayList();
-
-
-        QYMenuButton countMenu1 = new QYMenuButton();
-        countMenu1.setType(QYMenuType.CLICK);
-        countMenu1.setName("积分查询");
-        countMenu1.setKey("menu_countMenu1");
-        countMenus.add(countMenu1);
-
-        QYMenuButton countMenu2 = new QYMenuButton();
-        countMenu2.setType(QYMenuType.CLICK);
-        countMenu2.setName("积分兑换");
-        countMenu2.setKey("menu_countMenu1");
-        countMenus.add(countMenu2);
-
-        countMenu.setSubButton(countMenus);
-
         btns.add(noitceMenu);
-        btns.add(libMenu);
-        btns.add(countMenu);
         menu.setButton(btns);
         QYMenuAPI menuAPI = new QYMenuAPI(qyapiConfig);
         QYResultType resultType = menuAPI.create(menu, "7");
