ff9951245323fcbbdf4c5c2db1785f31e8d3cd1e
LIYR <eryanwcp@gmail.com>
2018-07-31 21:35:57 +0800

    去除事务注解

--- src/main/java/com/eryansky/modules/sys/service/AreaService.java
+++ src/main/java/com/eryansky/modules/sys/service/AreaService.java
@@ -10,14 +10,12 @@ import com.eryansky.common.orm.mybatis.interceptor.BaseInterceptor;
 import com.eryansky.core.orm.mybatis.service.TreeService;
 import com.eryansky.modules.sys.dao.AreaDao;
 import com.eryansky.modules.sys.mapper.Area;
-import com.eryansky.modules.sys.mapper.Organ;
 import com.eryansky.utils.AppConstants;
 import com.eryansky.utils.CacheConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
