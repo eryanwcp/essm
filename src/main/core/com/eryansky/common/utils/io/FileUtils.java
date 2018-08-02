a9ce95a00d0a59e2ad4d43aaa7a74613e374b862
eryan <eryanwcp@gmail.com>
2018-08-02 09:14:57 +0800

    Mybatis 自动加载

--- src/main/core/com/eryansky/common/utils/io/FileUtils.java
+++ src/main/core/com/eryansky/common/utils/io/FileUtils.java
@@ -352,5 +352,39 @@ public class FileUtils extends org.apache.commons.io.FileUtils {
 		}
 		return p;
 	}
-    
+
+	/**
+	 * 判断文件的编码格式
+	 *
+	 * @param file
+	 * @return 文件编码格式
+	 * @throws Exception
+	 */
+	public static String checkFileCodeString(File file) throws Exception {
+		if (file == null || !file.exists()) {
+			throw  new ServiceException("文件["+file.getAbsolutePath()+"]不存在");
+		}
+		BufferedInputStream bin = new BufferedInputStream(new FileInputStream(file));
+		int p = (bin.read() << 8) + bin.read();
+		String code = null;
+		//其中的 0xefbb、0xfffe、0xfeff、0x5c75这些都是这个文件的前面两个字节的16进制数
+		switch (p) {
+			case 0xefbb:
+				code = "UTF-8";
+				break;
+			case 0xfffe:
+				code = "Unicode";
+				break;
+			case 0xfeff:
+				code = "UTF-16BE";
+				break;
+			case 0x5c75:
+				code = "ANSI|ASCII";
+				break;
+			default:
+				code = "GBK";
+		}
+		return code;
+
+	}
 }
