d90b10b0ab84eca86c7a3561c6b40268d6ba0b25
eryan <eryanwcp@gmail.com>
2018-08-06 09:48:58 +0800

    jar

--- src/main/java/com/eryansky/modules/weixin/interceptor/QYWeixinOauth2Interceptor.java
+++ src/main/java/com/eryansky/modules/weixin/interceptor/QYWeixinOauth2Interceptor.java
@@ -1,17 +1,17 @@
 /**
- * Copyright (c) 2012-2018 http://www.eryansky.com
+ * Copyright (c) 2012-2014 http://www.eryansky.com
  * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  */
 package com.eryansky.modules.weixin.interceptor;
 
-import com.eryansky.fastweixin.company.api.QYUserAPI;
-import com.eryansky.fastweixin.company.api.config.QYAPIConfig;
-import com.eryansky.fastweixin.company.api.response.GetOauthUserInfoResponse;
 import com.eryansky.common.spring.SpringContextHolder;
 import com.eryansky.common.utils.StringUtils;
 import com.eryansky.common.utils.collections.Collections3;
 import com.eryansky.common.web.springmvc.SpringMVCHolder;
+import com.eryansky.fastweixin.company.api.QYUserAPI;
+import com.eryansky.fastweixin.company.api.config.QYAPIConfig;
+import com.eryansky.fastweixin.company.api.response.GetOauthUserInfoResponse;
 import com.google.common.collect.Lists;
 import com.eryansky.core.security.SecurityUtils;
 import com.eryansky.core.security.SessionInfo;
@@ -28,12 +28,11 @@ import java.util.List;
 
 
 /**
- * 微信Outho2认证拦截器
- * 优先级：注解>数据库权限配置
+ * 企业微信Outho2认证拦截器
  * @author 尔演&Eryan eryanwcp@gmail.com
  * @date 2015-01-21 12:23
  */
-public class WeixinOauth2Interceptor extends HandlerInterceptorAdapter {
+public class QYWeixinOauth2Interceptor extends HandlerInterceptorAdapter {
 
 
     protected Logger logger = LoggerFactory.getLogger(getClass());
@@ -51,6 +50,10 @@ public class WeixinOauth2Interceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
+        SessionInfo sessionInfo = SecurityUtils.getSessionInfo(SecurityUtils.getNoSuffixSessionId(request.getSession()));
+        if (sessionInfo != null) {
+            return true;
+        }
         String code = request.getParameter("code");
         String state = request.getParameter("state");
         if (StringUtils.isNotBlank(code) && StringUtils.isNotBlank(state)) {
@@ -71,15 +74,12 @@ public class WeixinOauth2Interceptor extends HandlerInterceptorAdapter {
                 QYUserAPI qyUserAPI = new QYUserAPI(qyapiConfig);
                 try {
                     GetOauthUserInfoResponse getOauthUserInfoResponse = qyUserAPI.getOauthUserInfo(code);
-                    SessionInfo sessionInfo = SecurityUtils.getSessionInfo(request.getSession().getId());
-                    if (sessionInfo == null) {
-                        User user = UserUtils.getUserByLoginName(getOauthUserInfoResponse.getUserid());
-                        if (user != null) {
-                            SecurityUtils.putUserToSession(SpringMVCHolder.getRequest(), user);//模拟自动登录
-                            logger.info("{} 自动登录", getOauthUserInfoResponse.getUserid());
-                        } else {
-                            logger.warn("{} not exist", getOauthUserInfoResponse.getUserid());
-                        }
+                    User user = UserUtils.getUserByLoginName(getOauthUserInfoResponse.getUserid());
+                    if (user != null) {
+                        SecurityUtils.putUserToSession(SpringMVCHolder.getRequest(), user);//模拟自动登录
+                        logger.info("{} 自动登录", getOauthUserInfoResponse.getUserid());
+                    } else {
+                        logger.warn("{} not exist", getOauthUserInfoResponse.getUserid());
                     }
                 } catch (Exception e) {
                     logger.error(e.getMessage(), e);
