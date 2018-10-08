9d8ca40992b5daca7af5e7bc7ffecb5fc87a8f39
eryan <eryanwcp@gmail.com>
2018-10-08 13:51:54 +0800

    IP获取方式增加

--- src/main/j2cache/com/eryansky/j2cache/session/J2CacheSessionFilter.java
+++ src/main/j2cache/com/eryansky/j2cache/session/J2CacheSessionFilter.java
@@ -15,12 +15,17 @@
  */
 package com.eryansky.j2cache.session;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Properties;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * 实现基于 J2Cache 的分布式的 Session 管理
@@ -36,6 +41,23 @@ public class J2CacheSessionFilter implements Filter {
     private int cookieMaxAge;
 
     private boolean discardNonSerializable;
+
+    private final Logger logger = LoggerFactory.getLogger(J2CacheSessionFilter.class);
+
+    private final String[] NULL_STRING_ARRAY = new String[0];
+    private final String URL_SPLIT_PATTERN = "[, ;\r\n]";//逗号  空格 分号  换行
+
+    private final PathMatcher pathMatcher = new AntPathMatcher();
+    /**
+     * 白名单
+     */
+    private String[] whiteListURLs = null;
+
+    /**
+     * 黑名单
+     */
+    private String[] blackListURLs = null;
+
 
     @Override
     public void init(FilterConfig config) {
@@ -55,10 +77,72 @@ public class J2CacheSessionFilter implements Filter {
         int maxSizeInMemory = Integer.parseInt(config.getInitParameter("session.maxSizeInMemory"));
 
         this.g_cache = new CacheFacade(maxSizeInMemory, this.cookieMaxAge, redisConf, this.discardNonSerializable);
+
+        String whiteListURLStr = config.getInitParameter("whiteListURL");
+        whiteListURLs = strToArray(whiteListURLStr);
+        String blackListURLStr = config.getInitParameter("blackListURL");
+        blackListURLs = strToArray(blackListURLStr);
+    }
+
+    private String[] strToArray(String urlStr) {
+        if (urlStr == null) {
+            return NULL_STRING_ARRAY;
+        }
+        String[] urlArray = urlStr.split(URL_SPLIT_PATTERN);
+
+        List<String> urlList = new ArrayList<String>();
+
+        for (String url : urlArray) {
+            url = url.trim();
+            if (url.length() == 0) {
+                continue;
+            }
+
+            urlList.add(url);
+        }
+
+        return urlList.toArray(NULL_STRING_ARRAY);
+    }
+
+    private boolean isWhiteURL(String currentURL) {
+        for (String whiteURL : whiteListURLs) {
+            if (pathMatcher.match(whiteURL, currentURL)) {
+                logger.debug("url filter : white url list matches : [{}] match [{}] continue", currentURL, whiteURL);
+                return true;
+            }
+        }
+        logger.debug("url filter : white url list not matches : [{}] not match [{}]",
+                currentURL, Arrays.toString(whiteListURLs));
+        return false;
+    }
+
+    private boolean isBlackURL(String currentURL) {
+        for (String blackURL : blackListURLs) {
+            if (pathMatcher.match(blackURL, currentURL)) {
+                logger.debug("url filter : black url list matches : [{}] match [{}] break", currentURL, blackURL);
+                return true;
+            }
+        }
+        logger.debug("url filter : black url list not matches : [{}] not match [{}]",
+                currentURL, Arrays.toString(blackListURLs));
+        return false;
     }
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) req;
+//        HttpServletResponse httpResponse = (HttpServletResponse) res;
+        String currentURL = httpRequest.getServletPath();
+        logger.debug("url filter : current url : [{}]", currentURL);
+        if (isBlackURL(currentURL)) {//黑名单
+            chain.doFilter(req, res);
+            return;
+        }
+        if (!isWhiteURL(currentURL)) {//白名单
+            chain.doFilter(req, res);
+            return;
+        }
+
         HttpServletRequest j2cacheRequest = new J2CacheRequestWrapper(req, res);
         try {
             chain.doFilter(j2cacheRequest, res);
