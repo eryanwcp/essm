407cdeb580cb99ea3f29c568636864ba70958d8a
eryan <eryanwcp@gmail.com>
2018-11-01 14:35:31 +0800

    微信Token 支持集群模式下获取

--- src/main/fastweixin/com/eryansky/fastweixin/cluster/j2cache/J2CacheAccessTokenCacheService.java
+++ src/main/fastweixin/com/eryansky/fastweixin/cluster/j2cache/J2CacheAccessTokenCacheService.java
@@ -8,6 +8,7 @@ import com.eryansky.j2cache.J2Cache;
 
 /**
  * Token缓存 J2Cache实现
+ *
  * @author 尔演&Eryan eryanwcp@gmail.com
  * @date 2018-10-31
  */
@@ -15,6 +16,7 @@ public class J2CacheAccessTokenCacheService implements IAccessTokenCacheService 
 
     private CacheChannel cache = J2Cache.getChannel();
     private String region = AccessTokenCache.CACHE_NAME;
+    private String prefix = "";
 
     public J2CacheAccessTokenCacheService() {
     }
@@ -24,9 +26,14 @@ public class J2CacheAccessTokenCacheService implements IAccessTokenCacheService 
     }
 
 
+    public J2CacheAccessTokenCacheService(String region, String prefix) {
+        this.region = region == null || "".equals(region.trim()) ? AccessTokenCache.CACHE_NAME:region;
+        this.prefix = prefix;
+    }
+
     @Override
     public AccessTokenCache getAccessTokenCache() {
-        CacheObject cacheObject = cache.get(region, AccessTokenCache.KEY_ACCESS_TOKEN_CACHE);
+        CacheObject cacheObject = cache.get(region, this.prefix + AccessTokenCache.KEY_ACCESS_TOKEN_CACHE);
         if (cacheObject != null) {
             return (AccessTokenCache) cacheObject.getValue();
         }
@@ -35,7 +42,7 @@ public class J2CacheAccessTokenCacheService implements IAccessTokenCacheService 
 
     @Override
     public void putAccessTokenCache(AccessTokenCache accessTokenCache) {
-        cache.set(region, AccessTokenCache.KEY_ACCESS_TOKEN_CACHE, accessTokenCache);
+        cache.set(region, this.prefix + AccessTokenCache.KEY_ACCESS_TOKEN_CACHE, accessTokenCache);
     }
 
 }
