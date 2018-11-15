cc587eae3189f267c36a9bafc6ee593c576e8365
eryan <eryanwcp@gmail.com>
2018-11-15 15:10:33 +0800

    工具优化

--- src/main/core/com/eryansky/common/utils/IdWorker.java
+++ src/main/core/com/eryansky/common/utils/IdWorker.java
@@ -1,5 +1,7 @@
 /**
- * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
+ *  Copyright (c) 2012-2018 http://www.eryansky.com
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
  */
 package com.eryansky.common.utils;
 
